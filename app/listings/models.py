c4a9eca3e0ee008de1d4c4288e2a5b84d702993f
Cyrille YAO <cyaoyapi@gmail.com>
2022-06-02 02:21:48 +0000

    Defining firts models and using them in views.

--- app/listings/models.py
+++ app/listings/models.py
@@ -1,3 +1,13 @@
+"""Module for models of the app listings."""
 from django.db import models
 
-# Create your models here.
+class Band(models.Model):
+    """Represent a band, a group of music."""
+
+    name = models.CharField("Nom", max_length=100)
+
+
+class Listing(models.Model):
+    """Represent an advertisement about an article of a group."""
+
+    title = models.CharField("Titre", max_length=100)
--- app/listings/views.py
+++ app/listings/views.py
@@ -2,10 +2,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from listings.models import Band, Listing
+
 def hello(request):
     """Say Hello Django!"""
-
-    return HttpResponse('<h1>Hello Django!</h1>')
+    
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django!</h1>
+        <h2>List of bands</h2>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+    """)
 
 def about(request):
     """Page to present us."""
@@ -15,7 +26,16 @@ def about(request):
 def listings(request):
     """Show list of advertisement."""
 
-    return HttpResponse('<h1>Listings</h1>')
+    listings = Listing.objects.all()
+    return HttpResponse(f"""
+        <h1>Listings</h1>
+        <h2>List of listings</h2>
+        <ul>
+            <li>{listings[0].title}</li>
+            <li>{listings[1].title}</li>
+            <li>{listings[2].title}</li>
+        </ul>
+    """)
 
 def contact(request):
     """Page to contact us."""
